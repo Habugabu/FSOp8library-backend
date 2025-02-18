6b31e1fd132444cc12ed4491d6dc44e6db5999cb
Habugabu <115887891+Habugabu@users.noreply.github.com>
2025-02-18 17:21:50 +0900

    8.6 completed

--- library-backend.js
+++ library-backend.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require("@apollo/server");
 const { startStandaloneServer } = require("@apollo/server/standalone");
+const { v1: uuid } = require("uuid");
 
 let authors = [
   {
@@ -96,7 +97,7 @@ let books = [
 const typeDefs = `
   type Book {
     title: String!
-    published: String!
+    published: Int!
     author: String!
     id: String!
     genres: [String!]!
@@ -104,6 +105,8 @@ const typeDefs = `
 
   type Author {
     name: String!
+    born: Int
+    id: String!
     bookCount: Int!
   }
 
@@ -112,6 +115,15 @@ const typeDefs = `
     authorCount: Int!
     allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
+  }
+
+  type Mutation {
+    addBook(
+      title: String!
+      published: Int!
+      author: String!
+      genres: [String!]!
+    ): Book
   }
 `;
 
@@ -136,6 +148,17 @@ const resolvers = {
   },
   Author: {
     bookCount: (root) => books.filter((b) => b.author === root.name).length,
+  },
+  Mutation: {
+    addBook: (root, args) => {
+      const book = { ...args, id: uuid() };
+      books = books.concat(book);
+      if (!authors.map((a) => a.name).includes(args.author)) {
+        const author = { name: args.author, id: uuid() };
+        authors = authors.concat(author);
+      }
+      return book;
+    },
   },
 };
 
