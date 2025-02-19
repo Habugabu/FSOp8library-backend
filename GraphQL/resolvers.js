44a463bd51859ddd19670fca97d6ab00f21fd241
Habugabu <115887891+Habugabu@users.noreply.github.com>
2025-02-19 19:47:31 +0900

    refactor

---
+++ GraphQL/resolvers.js
@@ -0,0 +1,139 @@
+const { GraphQLError } = require("graphql");
+const jwt = require("jsonwebtoken");
+const Book = require("../models/book");
+const Author = require("../models/author");
+const User = require("../models/user");
+
+const resolvers = {
+  Query: {
+    bookCount: async () => Book.collection.countDocuments(),
+    authorCount: async () => Author.collection.countDocuments(),
+    allBooks: async (root, args) => {
+      if (!args.author && !args.genre) {
+        return Book.find({});
+      } else if (!args.genre) {
+        const author = await Author.findOne({ name: args.author });
+        if (!author) {
+          return [];
+        } else {
+          return Book.find({ author: author._id });
+        }
+      } else if (!args.author) {
+        return Book.find({ genres: args.genre });
+      } else {
+        const author = await Author.findOne({ name: args.author });
+        if (!author) {
+          return [];
+        } else {
+          return Book.find({ author: author._id, genres: args.genre });
+        }
+      }
+    },
+    allAuthors: async () => Author.find({}),
+    me: (root, args, context) => {
+      return context.currentUser;
+    },
+  },
+  Book: {
+    author: async (root) => Author.findById(root.author),
+  },
+  Author: {
+    bookCount: async (root) => {
+      const books = await Book.find({ author: root._id });
+      return books.length;
+    },
+  },
+  Mutation: {
+    addBook: async (root, args, context) => {
+      const currentUser = context.currentUser;
+      if (!currentUser) {
+        throw new GraphQLError("not authenticated", {
+          extensions: { code: "BAD_USER_INPUT" },
+        });
+      }
+      const author = await Author.findOne({ name: args.authorName });
+      console.log(author);
+      let book = {};
+      if (!author) {
+        const newAuthor = new Author({ name: args.authorName });
+        try {
+          await newAuthor.save();
+        } catch (error) {
+          throw new GraphQLError("Saving book failed", {
+            extensions: {
+              code: "BAD_USER_INPUT",
+              error,
+            },
+          });
+        }
+        book = new Book({ ...args, author: newAuthor._id });
+      } else {
+        book = new Book({ ...args, author: author._id });
+      }
+      try {
+        await book.save();
+      } catch (error) {
+        throw new GraphQLError("Saving book failed", {
+          extensions: {
+            code: "BAD_USER_INPUT",
+            error,
+          },
+        });
+      }
+      return book;
+    },
+    editAuthor: async (root, args, context) => {
+      const currentUser = context.currentUser;
+      if (!currentUser) {
+        throw new GraphQLError("not authenticated", {
+          extensions: { code: "BAD_USER_INPUT" },
+        });
+      }
+      try {
+        const author = await Author.findOne({ name: args.name });
+        author.born = args.setBornTo;
+        return author.save();
+      } catch (error) {
+        throw new GraphQLError("Changing birthyear failed", {
+          extensions: {
+            code: "BAD_USER_INPUT",
+            error,
+          },
+        });
+      }
+    },
+    createUser: async (root, args) => {
+      const user = new User({ ...args });
+
+      return user.save().catch((error) => {
+        throw new GraphQLError("Creating the user failed", {
+          extensions: {
+            code: "BAD_USER_INPUT",
+            invalidArgs: args.username,
+            error,
+          },
+        });
+      });
+    },
+    login: async (root, args) => {
+      const user = await User.findOne({ username: args.username });
+
+      if (!user || args.password !== "secret") {
+        throw new GraphQLError("wrong credentials", {
+          extensions: {
+            code: "BAD_USER_INPUT",
+          },
+        });
+      }
+
+      const userForToken = {
+        username: user.username,
+        id: user._id,
+      };
+
+      return { value: jwt.sign(userForToken, process.env.JWT_SECRET) };
+    },
+  },
+};
+
+module.exports = resolvers;
